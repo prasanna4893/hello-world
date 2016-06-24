358b3fd2a6d66b31f1a95cdc240b9122ebfd88cb
prasanna4893 <prasanna4893@gmail.com>
2016-06-24 13:50:54 -0700

    Update fibo.c

--- fibo.c
+++ fibo.c
@@ -3,11 +3,11 @@
 
 int main()
 {
-   int n=10, first = 0, second = 1, next, c;
+   int n, first = 0, second = 1, next, c;
 
   // printf("Enter the number of terms\n");
-   //printf("Enter the number of terms\n");
-   //scanf("%d",&n);
+   printf("Enter the number of terms\n");
+   scanf("%d",&n);
       printf("First %d terms of Fibonacci series are :=\n",n);
    for ( c = 0 ; c < n ; c++ )
    {
